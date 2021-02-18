14456e0a3575bfe4307ea844224dba8d4861a7ee
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-02-17 19:16:35 -0700

    asterusing alias event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   post = {
-    title : 'Title',
-    isFavorite : true
+    title: 'Title',
+    isFavorite: true
   }
 }
