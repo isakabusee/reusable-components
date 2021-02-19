967b46cec111f9f9b13446aa9250fc0710d06c3b
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-02-18 19:40:16 -0700

    asterraising custom events

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'favorite',
@@ -7,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
   @Input('isFavorite') isSelected: boolean;
+  @Output() change = new EventEmitter();
 
   constructor() { }
 
@@ -15,6 +17,7 @@ export class FavoriteComponent implements OnInit {
 
   onClick() {
     this.isSelected = !this.isSelected;
+    this.change.emit();
   }
 
 }
